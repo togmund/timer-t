03719ff365aec08a506b7203a3c2799819602795
Ted Ogmundson <togmund@gmail.com>
2019-10-23 01:01:20 +0000

    excluded non-numbers from booping, implemeneted a spinner that spins while waiting for a boop, implemented a boop! string that fires when the boop fires, implemented an exit message.

--- timer2.js
+++ timer2.js
@@ -6,8 +6,8 @@ Implement a new timer which behaves in the following way:
 
 - The user can press b and it should beep right away
 
-- The user can input any number from 1 to 9 and it should 
-  - immediately output "setting timer for x seconds...", 
+- The user can input any number from 1 to 9 and it should
+  - immediately output "setting timer for x seconds...",
   - and beep after that number of seconds has passed
 
 - The user can use ctrl + c to exit the program, at which point the program should say "Thanks for using me, ciao!" before terminating
@@ -18,21 +18,40 @@ Implement a new timer which behaves in the following way:
 // const nodeArgs = process.argv.slice(2).filter(elm => parseInt(elm));
 
 // INIT user inputs
-const stdin = process.stdin
-stdin.setRawMode(true)
-stdin.setEncoding('utf8')
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
 
 // Rules of the program
 const ruleBook = {
   "b": "beep immediately",
+  "1-9": "beep after ",
   "Ctrl+C": "exit"
-}
+};
 
 // Function to print the rules of the program for the user
-const printMenu = (rules) => {
+const printRules = (rules) => {
   for (const key in rules) {
-    console.log(`  • ${key} to ${rules[key]}`)
+    console.log(`  • ${key} to ${rules[key]}`);
   }
+};
+
+// Spinner Implementation to spice up the timer
+const spinner2 = () => {
+  const sprites =
+  [ '|'
+  , '/'
+  , '-'
+  ,'\\'
+  , '|'
+  ]
+  for (let rad = 0; rad < sprites.length; rad++) {
+    setTimeout(() => { process.stdout.write(`\r${sprites[rad]}    `); }, rad * 200);
+  }
+}
+
+const spinnerCaller = (spinnerFunc, cycles) => {
+  for (let cyc = 0; cyc < cycles; cyc++) { setTimeout(() => { spinnerFunc(); }, cyc * 1000); }
 }
 
 // Function that beeps as many times as you request
@@ -40,19 +59,27 @@ const beeper = (beeps) => {
   for (const beep of beeps) {
     setTimeout(() => {
       process.stdout.write('\x07');
+      console.log('\rboop!')
     }, beep * 1000);
   }
 };
 
-// stdin 
+// stdin
 stdin.on('data', (data) => {
   if (data === '\u0003') {
+    console.log("Thanks for using me, ciao!");
     process.exit();
   }
   if (data === "b") {
     beeper([0]);
   }
+  if (0 < parseInt(data) < 10 && !Number.isNaN(parseInt(data))) {
+    beeper([data]);
+    spinnerCaller(spinner2, data);
+    console.log(`setting timer for ${data} seconds...`);
+  }
 });
 
 console.log("Welcome to Beepertown");
-printMenu(ruleBook);
+printRules(ruleBook);
+console.log("Press if you dare")
